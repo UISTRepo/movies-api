30e0997adbcf7b495ed3663f806f7b3dde44eab2
tuntev <tuntev@gmail.com>
2022-04-27 15:26:42 +0200

    Added rate limiter to 60 req/min

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 var authRouter = require('./routes/auth');
 var moviesRouter = require('./routes/movies');
@@ -14,7 +15,14 @@ dotenv.config();
 
 var app = express();
 
-app.use('/api', require('cors')());
+const apiLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 60, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use('/api', require('cors')(), apiLimiter);
 
 app.use(logger('dev'));
 app.use(express.json());
